48b5d35571dc1a6670ec55a600502d7579555661
Si Chen <schen@Sis-MacBook-Pro.local>
2014-10-13 23:28:34 +0100

    Subtract, Multiple, Divide functionality working.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,17 +1,63 @@
+
+
 public class Calculator {
 
 	public double evaluate(String expression) {
 		//double result = Double.parseDouble(expression);
 		//return result;
 		double single_number;
-		double result;
+		double result = 0.0;
+		String[] numbers;
+
 		
 		if (expression.length() == 1) {
 			single_number = Double.parseDouble(expression);
 			return single_number;
 		} else {
-			result = Double.valueOf(expression.substring(0, expression.indexOf("+"))) + Double.valueOf(expression.substring(expression.indexOf("+") + 1, expression.length()));
+			
+			if (expression.contains("+") && !expression.contains("-") && !expression.contains("*") && !expression.contains("/")) {
+				numbers = expression.split("\\+");
+				
+				for (int i=0; i<numbers.length; i++) {
+					result += Double.parseDouble(numbers[i]);
+				}
+				return result;
+			}
+			
+			if (expression.contains("-") && !expression.contains("+") && !expression.contains("*") && !expression.contains("/")) {
+				numbers = expression.split("-");
+				result = Double.parseDouble(numbers[0]);
+				
+				for (int i=1; i<numbers.length; i++) {
+					result -= Double.parseDouble(numbers[i]);
+				}
+				return result;
+			}
+			
+			if (expression.contains("*") && !expression.contains("+") && !expression.contains("-") && !expression.contains("/")) {
+				numbers = expression.split("\\*");
+				result = 1.0;
+				
+				for (int i=0; i<numbers.length; i++) {
+					result *= Double.parseDouble(numbers[i]);
+				}
+				return result;
+			}
+			
+			if (expression.contains("/") && !expression.contains("+") && !expression.contains("-") && !expression.contains("*")) {
+				numbers = expression.split("\\/");
+				result = Double.parseDouble(numbers[0]);
+				
+				for (int i=1; i<numbers.length; i++) {
+					result = result / Double.parseDouble(numbers[i]);
+				}
+				return result;
+			}
+
+
+			
 			return result;
+			
 		}
 	}
 	
@@ -19,7 +65,17 @@ public class Calculator {
 		
 		Calculator c = new Calculator();
 		
-		System.out.println(c.evaluate("255+1450"));
+		System.out.println(c.evaluate("80/4"));
+		
+	/*	String test = "2/6/4-1";
+		
+	String[] r = test.split("\\*|\\/|\\+|-");
+		
+		for (int i =0; i< r.length; i++) {
+			
+			System.out.println(r[i]);
+		}*/
+		
 	}
 	
 }
--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -6,6 +6,7 @@ public class CalculatorTest {
 
 	Calculator calc = new Calculator();
 	
+	/**Single Number**/
 	@Test
 	public void testEvaluateReturnsDoubleFourWhenStringFourIsPassedIn() {
 		String expression = "4";
@@ -18,6 +19,7 @@ public class CalculatorTest {
 		assertEquals(5.0, calc.evaluate(expression), 0.0);
 	}
 	
+	/**Add two numbers**/
 	@Test
 	public void testEvaluateReturnsDoubleFourWhenStringTwoPlusTwoIsPassedIn() {
 		String expression = "2+2";
@@ -31,4 +33,62 @@ public class CalculatorTest {
 		assertEquals(5.0, calc.evaluate(expression), 0.0);
 	
 	}
+	
+	/**Add more than 2 numbers**/
+	@Test
+	public void testEvaluateReturnsDoubleTenWhenStringTwoPlusThreePlusFiveIsPassedIn() {
+		String expression = "2+3+5";
+		assertEquals(10.0, calc.evaluate(expression), 0.0);
+	}
+	
+	
+	// Subtract - two numbers 
+	@Test
+	public void testEvaluateReturnsDoubleFourWhenStringEightMinusFourIsPassedIn() {
+		String expression = "8-4";
+		assertEquals(4.0, calc.evaluate(expression), 0.0);
+	
+	}
+	
+	// Subtract - more than two numbers
+	@Test
+	public void testEvaluateReturnsDoubleFiveWhenStringTenMinusThreeMinusTwoIsPassedIn() {
+		String expression = "10-3-2";
+		assertEquals(5.0, calc.evaluate(expression), 0.0);
+	}
+	
+	// Multiply two numbers
+	@Test
+	public void testEvaluateReturnsDoubleTwentyFourWhenStringSixMultiplyFourIsPassedIn() {
+		String expression = "6*4";
+		assertEquals(24.0, calc.evaluate(expression), 0.0);
+	
+	}
+	
+	// Multiply more than two numbers
+	@Test
+	public void testEvaluateReturnsDoubleThirtyWhenStringTwoMultiplyThreeMultiplyFiveIsPassedIn() {
+		String expression = "2*3*5";
+		assertEquals(30.0, calc.evaluate(expression), 0.0);
+	
+	}
+	
+	// Divide - two numbers
+	@Test
+	public void testEvaluateReturnsDoubleThreeWhenStringNineDivideThreeIsPassedIn() {
+		String expression = "9/3";
+		assertEquals(3.0, calc.evaluate(expression), 0.0);
+	
+	}
+	
+	@Test
+	public void testEvaluateReturnsDoubleTenWhenStringEightyDivideFourDivideTwoIsPassedIn() {
+		String expression = "80/4/2";
+		assertEquals(10.0, calc.evaluate(expression), 0.0);
+	
+	}
+	
+	
+	
+	
 }
