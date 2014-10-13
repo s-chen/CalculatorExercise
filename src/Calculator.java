81a0321eccd1de9f79fb678ec955c4fe72d2ad8e
Si Chen <schen@Sis-MacBook-Pro.local>
2014-10-13 20:53:10 +0100

    Add functionality for 2 numbers working.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,6 @@
 public class Calculator {
 
-	public static double evaluate(String expression) {
+	public double evaluate(String expression) {
 		//double result = Double.parseDouble(expression);
 		//return result;
 		double single_number;
@@ -10,19 +10,16 @@ public class Calculator {
 			single_number = Double.parseDouble(expression);
 			return single_number;
 		} else {
-			result = Double.parseDouble(String.valueOf(expression.charAt(0)));
-			for (int i=1; i < expression.length(); i++) {
-				if (String.valueOf(expression.charAt(i)) == "+") {
-					result += Double.valueOf(expression.charAt(i+1));
-				}
-			}
+			result = Double.valueOf(expression.substring(0, expression.indexOf("+"))) + Double.valueOf(expression.substring(expression.indexOf("+") + 1, expression.length()));
 			return result;
 		}
 	}
 	
 	public static void main(String[] args){
 		
-		System.out.println(evaluate("2+2"));
+		Calculator c = new Calculator();
+		
+		System.out.println(c.evaluate("255+1450"));
 	}
 	
 }
